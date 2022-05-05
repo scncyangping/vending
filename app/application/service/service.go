70b291224aa7dd907c8bbf9d47701a4f3545187a
Yapi <hero.yp@qq.com>
2022-05-05 21:56:25 +0800

    fix

--- app/application/service/service.go
+++ app/application/service/service.go
@@ -10,6 +10,20 @@ type AuthSrv interface {
 	Register(*dto.RegisterRe) (string, error)
 }
 
+type CommoditySrv interface {
+	// 上架商品
+	Up([]string) error
+	// 下架商品
+	Down([]string) error
+	// 导入商品
+	Import(string) (count int, err error)
+}
+
+type OrderSrv interface {
+	// CreateOrder 创建订单 返回付款url
+	CreateOrder(interface{}) (string, error)
+}
+
 //
 //type SrvManager struct {
 //	AuthSrv
