905f419ff20185d53c9004ac2fd3bed67bd354c3
Yapi <hero.yp@qq.com>
2022-05-06 15:57:39 +0800

    commodity

---
+++ app/application/service/impl/commodity_imp.go
@@ -0,0 +1,20 @@
+package impl
+
+type CommoditySrvImp struct {
+}
+
+func (c *CommoditySrvImp) Up(strings []string) error {
+	panic("implement me")
+}
+
+func (c *CommoditySrvImp) Down(strings []string) error {
+	panic("implement me")
+}
+
+func (c *CommoditySrvImp) Delete(strings []string) error {
+	panic("implement me")
+}
+
+func (c *CommoditySrvImp) Import(i interface{}) (count int, err error) {
+	panic("implement me")
+}
