ee5f99f123f8a73555f8ac4d644ee0719cee36d1
Yapi <hero.yp@qq.com>
2022-05-12 10:11:54 +0800

    obj defined

--- app/domain/obj/obj.go
+++ app/domain/obj/obj.go
@@ -1,1 +1,8 @@
 package obj
+
+// OrderItemObj 订单明细
+type OrderItemObj struct {
+	CommodityName  string  `json:"commodityName"`  // 商品类别+商品名称
+	OriginalAmount float64 `json:"originalAmount"` // 商品原金额
+	Amount         float64 `json:"amount"`         // 折扣计算后金额
+}
