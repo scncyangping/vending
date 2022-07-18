add7b84518accbf7b1ef559b6b75b180d445e54e
Yapi <hero.yp@qq.com>
2022-07-18 22:14:34 +0800

    desc

--- app/infrastructure/pkg/util/date.go
+++ app/infrastructure/pkg/util/date.go
@@ -4,7 +4,7 @@ import (
 	"time"
 )
 
-// 当前时间年月日字符串
+// NowDateFormat 当前时间年月日字符串
 func NowDateFormat() string {
 	t := time.Now()
 	return t.Format("2006-01-02")
@@ -20,18 +20,18 @@ func DateTimeFormatCustom(format string) string {
 	return t.Format(format)
 }
 
-// 当前时间戳
+// NowTimestamp 当前时间戳
 func NowTimestamp() int64 {
 	return time.Now().Unix()
 }
 
-// 时间戳转年月日时分秒字符串
+// TimeFormat 时间戳转年月日时分秒字符串
 func TimeFormat(ts int64) string {
 	t := time.Unix(ts, 0)
 	return t.Format("2006-01-02 15:04:05")
 }
 
-// 时间戳转自定义字符串
+// TimeFormatCustom 时间戳转自定义字符串
 func TimeFormatCustom(ts int64, format string) string {
 	t := time.Unix(ts, 0)
 	return t.Format(format)
